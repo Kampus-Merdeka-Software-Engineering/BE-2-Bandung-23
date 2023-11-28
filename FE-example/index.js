a1dd194ae9d35677be3cfdcc18ef14c22e8f245d
rdwnalawi <148617591+rdwnalawi@users.noreply.github.com>
2023-11-28 07:58:52 +0700

    update server

---
+++ FE-example/index.js
@@ -0,0 +1,19 @@
+// CONNECT TO BACKEND SERVER
+const API_URL = 'http://localhost:3000';
+
+async function getMenu() {
+  try {
+    const response = await fetch(`${API_URL}/offer`);
+    const menus = await response.json();
+    const menuOffer = document.getElementById('card-offer');
+    menus.forEach((menu) => {
+      const newMenu = document.createElement('p');
+      newMenu.textContent = `${menu.menu_name}`;
+      menuOffer.appendChild(newMenu);
+    })
+  } catch (error) {
+    console.log(menus);
+  }
+}
+
+getMenu();
--- index.js
+++ index.js
@@ -2,33 +2,28 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const cors = require('cors');
-const { error } = require('console');
-const PORT = 3000;
+const port = 3000;
 
 app.use(cors());
-
-// main route
+ 
 app.get('/', (req, res) => {
-    res.send('Welcomd to Our Web');
+  res.send('Hello World!');
 });
 
-// route "Penawaran Kami"
 app.get('/offer', (req, res) => {
-    fs.readFile('/menu.json', (error, data) => {
-        if (error) res.send ('Gagal memuat data');
-        const menu = JSON.parse(data);
-        res.status(200).send(menu);
-    })
+  fs.readFile('./menus.json', (error, data) => {
+    if (error) res.send ('Gagal dalam memuat data');
+    const menus = JSON.parse(data)
+    res.status(200).send(menus);
+  })  
 });
 
-// wrong route 
 app.all('*', (req, res) => {
     res.status(404).send('404 routes not found');
 });
-
-// server on
-app.listen(PORT, () => {
-    console.log(`Example app listening at https://kampus-merdeka-software-engineering.github.io/FE-2-Bandung-23/:${PORT}`)
+ 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
 });
 
 
