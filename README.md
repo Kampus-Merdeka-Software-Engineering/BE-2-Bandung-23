53d9d099b6129bfb4e5bbb2038c6e8972fb259fe
Yackis <101496534+Yackis@users.noreply.github.com>
2023-12-04 23:23:49 +0700

    delete

--- README.md
+++
@@ -1,3 +0,0 @@
-# BE-2-Bandung-23
-BE-2-section-bandung-group-23 created by GitHub Classroom
-created for Company Profile Seribu Rasa
--- prisma/migrations/20231203140252_change_data_type_float_to_int/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE `menu` MODIFY `menu_price` INTEGER NOT NULL;
-
--- AlterTable
-ALTER TABLE `transaction` MODIFY `subtotal` INTEGER NOT NULL,
-    MODIFY `shipping_cost` INTEGER NOT NULL,
-    MODIFY `total` INTEGER NOT NULL;
